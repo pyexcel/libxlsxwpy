234b6f39f54d4718596a627cd023a538411c8af3
chfw <wangc_2011@hotmail.com>
2020-10-13 22:09:51 +0100

    :green_heart: restore test script

--- test.bat
+++ test.bat
@@ -1,3 +1,3 @@
-#/bin/bash
 pip freeze
-nosetests --with-coverage --cover-package libxlsxwpy --cover-package tests tests --with-doctest --doctest-extension=.rst README.rst docs/source 
+nosetests --with-cov --cover-package libxlsxwpy --cover-package tests --with-doctest --doctest-extension=.rst tests README.rst
+
--- test.sh
+++ test.sh
@@ -1,3 +1,2 @@
-#/bin/bash
 pip freeze
-nosetests --with-coverage --cover-package libxlsxwpy --cover-package tests tests --with-doctest --doctest-extension=.rst README.rst docs/source 
+nosetests --with-coverage --cover-package libxlsxwpy --cover-package tests --with-doctest --doctest-extension=.rst tests README.rst
